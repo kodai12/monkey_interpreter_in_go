af58655fdea30daf26fb71c7671ff9fd5ce51bd1
sakochik <sakochi.kodai1209@gmail.com>
2018-07-15 18:07:00 +0900

    fix: ast => letに対応

--- ast/ast.go
+++ ast/ast.go
@@ -27,3 +27,20 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
