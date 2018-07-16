33099240f39021c4f650d898bb015756ca4b5071
sakochik <sakochi.kodai1209@gmail.com>
2018-07-16 17:18:23 +0900

    add: object => null

--- object/object.go
+++ object/object.go
@@ -5,6 +5,7 @@ import "fmt"
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type ObjectType string
@@ -22,8 +23,13 @@ type Boolean struct {
 	Value bool
 }
 
+type Null struct{}
+
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
