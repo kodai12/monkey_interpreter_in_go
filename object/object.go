f60ad32105a90f2832183303885ae769370d11da
sakochik <sakochi.kodai1209@gmail.com>
2018-07-16 17:15:33 +0900

    add: object => 真偽値

--- object/object.go
+++ object/object.go
@@ -4,6 +4,7 @@ import "fmt"
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
 type ObjectType string
@@ -17,10 +18,12 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Type() ObjectType {
-	return INTEGER_OBJ
+type Boolean struct {
+	Value bool
 }
 
-func (i *Integer) Inspect() string {
-	return fmt.Sprintf("%d", i.Value)
-}
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
